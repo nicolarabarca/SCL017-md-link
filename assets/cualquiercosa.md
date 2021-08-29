14e4b93b1c028655961d4250436289857f632895
ni.colara@hotmail.com <ni.colara@hotmail.com>
2021-08-29 18:58:51 -0400

    reconoce estados de url ok o fail

--- assets/cualquiercosa.md
+++ assets/cualquiercosa.md
@@ -3,3 +3,41 @@ Markdown es un lenguaje de marcado ligero muy popular entre developers. Es usado
 Estos archivos Markdown normalmente contienen links (vínculos/ligas) que muchas veces están rotos o ya no son válidos y eso perjudica mucho el valor de la información que se quiere compartir.
 
 Dentro de una comunidad de código abierto, nos han propuesto crear una herramienta usando Node.js, que lea y analice archivos en formato Markdown, para verificar los links que contengan y reportar algunas estadísticas.
+
+# Foobar
+
+Foobar is a Python library for dealing with word pluralization.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install foobar
+```
+
+## Usage
+
+```python
+import foobar
+
+# returns 'words'
+foobar.pluralize('word')
+
+# returns 'geese'
+foobar.pluralize('goose')
+
+# returns 'phenomenon'
+foobar.singularize('phenomena')
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## url  mala
+[MIT](https://fakeurl1.com)
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
+const fetch = require("node-fetch");
 
 mdLinks('./assets');
 
 
 function mdLinks(path) {
+  let linksToReview = []; // se guardan todos los link del archivo md que se encuentran
+  let linksOK = [];// todos los links buenos
+  let linksNOOK = [];// todos los link malos 
   try {
     const isDirectory = fs.lstatSync(path).isDirectory(); // "variable que pasa a argumento"
     console.log(isDirectory);
@@ -11,15 +15,85 @@ function mdLinks(path) {
       fs.readdir(path, (err, files) => {
         files.forEach((file) => {
           if (file.includes('.md')) {
-            fs.readFile(path+"/"+file, "utf-8", (err, data) => {
-             // const lines = data.split(/\r?\n/);
-            console.log(data);
-          });
+           
+
+            fs.readFile(path+"/"+file, "utf-8", (err, data) => {// se concatenzz el nombre del archivo mas la ruta
+              const lines = data.split(/\r?\n/);
+              lines.forEach((line) => {
+                let  hasLinks = line.indexOf('http://') !== -1 || line.indexOf('https://') !== -1
+                if (hasLinks) {
+                  linksToReview.push(getUrl(line));
+                }
+              });
+              //console.log("linksToReview.toString()");
+              //console.log(linksToReview.toString());
+              if(linksToReview.length > 0){
+                for(let link of linksToReview){
+                  fetch(link).then(function(response) {// fetch ve  el estado del link si es  ok o fail
+                    if(response.ok) {
+                      console.log(link + " OK");
+                    } else {
+                      console.log(link + "FAIL");
+                    }
+                  })
+                  .catch(function(error) {
+                    console.log(link + " FAIL");
+                  });
+
+
+                }
+
+              }
+       
+            });            
           }
          
         });
       });
-}
+    }else{//Si no es directorio es un archivo, por lo cual aqui el proceso 
+          //Pasa compobrar el nombre del archivo y leer el archivo con la funcion fs.readFile
+
+          if (path.includes('.md')) {
+           
+            fs.readFile(path, "utf-8", (err, data) => {
+              const lines = data.split(/\r?\n/);
+              lines.forEach((line) => {
+                var hasLinks = line.indexOf('http://') !== -1 || line.indexOf('https://') !== -1
+                if (hasLinks) {
+                  linksToReview.push(getUrl(line));
+                }
+              });
+              //console.log("linksToReview.toString()");
+              //console.log(linksToReview.length);
+              if(linksToReview.length > 0){
+                for(let link of linksToReview){
+                  fetch(link).then(function(response) {
+                    if(response.ok) {
+                      linksOK.push((link));
+                    } else {
+                      linksNOOK.push((link));
+                    }
+                  })
+                  .catch(function(error) {
+                    linksNOOK.push((link));
+                  });
+
+                }
+
+              }
+              console.log("linksOK.toString()");
+              console.log(linksOK.toString());
+              console.log("linksNOOK.toString()");
+              console.log(linksNOOK.toString());
+            });            
+          }          
+
+
+    }
+
+
+
+
   } catch (e) {
     // Handle error
     if (e.code == "ENOENT") {
@@ -32,29 +106,33 @@ function mdLinks(path) {
 }
 
 
+function getUrl(line){
+
+  if (line.indexOf('http://') !== -1) { // se  usa  para  limpiar  la  linea  se agrega  un  if  para  limpiar  en el caso de  que despues de la  primera  vez que limpie  quede  con  un ()al lado derecho
+    //console.log('line http: ', line)
+    let partialLink = line.substring(line.indexOf('http://'), line.length );
+    if(partialLink.indexOf(')') > -1){
+      return  partialLink.substring(partialLink.indexOf('http://'), partialLink.indexOf(')') );
+    }else{
+      return partialLink;
+    }
+    
+  }
+  if (line.indexOf('https://') !== -1) {
+    let partialLink = line.substring(line.indexOf('https://'), line.length);
+    if(partialLink.indexOf(')') > -1){
+      return  partialLink.substring(partialLink.indexOf('https://'), partialLink.indexOf(')') );
+    }else{
+      return partialLink;
+    }
+    
+  }
 
 
-/*let linksToReview = []
+  
 
-fs.readFile("README.md", "utf-8", (err, data) => {
-  const lines = data.split(/\r?\n/);
-  lines.forEach((line) => {
-    var hasLinks = line.indexOf('http://') !== -1 || line.indexOf('https://') !== -1
-    if (hasLinks) {
-      if (line.indexOf('http://') !== -1) {
-        console.log('line http: ', line)
-        let partialLink = line.substring(line.indexOf('http://'), line.length - 1)
-        let finalLink = partialLink.substring(line.indexOf('http://'), line.indexOf(' ') - 1)
-        console.log('finalLink http: ', finalLink)
-        linksToReview.push(finalLink)
-      }
-      if (line.indexOf('https://') !== -1) {
-        console.log('line https: ', line)
-        let partialLink = line.substring(line.indexOf('https://'), line.length - 1)
-        let finalLink = partialLink.substring(line.indexOf('https://'), line.indexOf(')') - 1)
-        console.log('finalLink https: ', finalLink)
-        linksToReview.push(finalLink)
-      }
-    }; //  borrar  punto y coma
-  });
-});*/
+}
+
+
+
+
